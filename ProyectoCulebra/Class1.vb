30a2dce463e9cbb5e15714c15302d507fe9b3c7b
FSC <santosarenillas@hotmail.com>
2016-10-20 00:15:14 +0200

    Commit1

---
+++ ProyectoCulebra/Class1.vb
@@ -0,0 +1,4 @@
+﻿Public Class Class1
+    Dim A As Integer
+
+End Class
--- ProyectoCulebra/Form1.vb
+++ ProyectoCulebra/Form1.vb
@@ -1,3 +1,18 @@
 ﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Beep()
+    End Sub
 
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.BackColor = Color.LightGoldenrodYellow
+
+
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Beep()
+        Beep()
+        Beep()
+    End Sub
 End Class
